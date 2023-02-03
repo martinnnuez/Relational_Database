eac7970bf888f0db3c4ebb201227576351273aed
Martin <64661515+martinnnuez@users.noreply.github.com>
2023-02-03 12:07:54 -0300

    Update periodic_table.sql

--- Periodic_Table/periodic_table.sql
+++ Periodic_Table/periodic_table.sql
@@ -62,10 +62,11 @@ ALTER TABLE public.elements OWNER TO freecodecamp;
 
 CREATE TABLE public.properties (
     atomic_number integer NOT NULL,
+    metal character varying(30),
     atomic_mass numeric NOT NULL,
     melting_point_celsius numeric NOT NULL,
     boiling_point_celsius numeric NOT NULL,
-    type_id integer NOT NULL
+    type_id integer DEFAULT 1 NOT NULL
 );
 
 
@@ -82,35 +83,6 @@ CREATE TABLE public.types (
 
 
 ALTER TABLE public.types OWNER TO freecodecamp;
-
---
--- Name: types_type_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
---
-
-CREATE SEQUENCE public.types_type_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.types_type_id_seq OWNER TO freecodecamp;
-
---
--- Name: types_type_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
---
-
-ALTER SEQUENCE public.types_type_id_seq OWNED BY public.types.type_id;
-
-
---
--- Name: types type_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
---
-
-ALTER TABLE ONLY public.types ALTER COLUMN type_id SET DEFAULT nextval('public.types_type_id_seq'::regclass);
-
 
 --
 -- Data for Name: elements; Type: TABLE DATA; Schema: public; Owner: freecodecamp
@@ -132,16 +104,16 @@ INSERT INTO public.elements VALUES (10, 'Ne', 'Neon');
 -- Data for Name: properties; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.properties VALUES (1, 1.008, -259.1, -252.9, 1);
-INSERT INTO public.properties VALUES (2, 4.0026, -272.2, -269, 1);
-INSERT INTO public.properties VALUES (3, 6.94, 180.54, 1342, 2);
-INSERT INTO public.properties VALUES (4, 9.0122, 1287, 2470, 2);
-INSERT INTO public.properties VALUES (5, 10.81, 2075, 4000, 3);
-INSERT INTO public.properties VALUES (6, 12.011, 3550, 4027, 1);
-INSERT INTO public.properties VALUES (7, 14.007, -210.1, -195.8, 1);
-INSERT INTO public.properties VALUES (8, 15.999, -218, -183, 1);
-INSERT INTO public.properties VALUES (9, 18.998, -220, -188.1, 1);
-INSERT INTO public.properties VALUES (10, 20.18, -248.6, -246.1, 1);
+INSERT INTO public.properties VALUES (1, 'nonmetal', 1.008, -259.1, -252.9, 1);
+INSERT INTO public.properties VALUES (2, 'nonmetal', 4.0026, -272.2, -269, 1);
+INSERT INTO public.properties VALUES (3, 'metal', 6.94, 180.54, 1342, 1);
+INSERT INTO public.properties VALUES (4, 'metal', 9.0122, 1287, 2470, 1);
+INSERT INTO public.properties VALUES (5, 'metalloid', 10.81, 2075, 4000, 1);
+INSERT INTO public.properties VALUES (6, 'nonmetal', 12.011, 3550, 4027, 1);
+INSERT INTO public.properties VALUES (7, 'nonmetal', 14.007, -210.1, -195.8, 1);
+INSERT INTO public.properties VALUES (8, 'nonmetal', 15.999, -218, -183, 1);
+INSERT INTO public.properties VALUES (9, 'nonmetal', 18.998, -220, -188.1, 1);
+INSERT INTO public.properties VALUES (10, 'nonmetal', 20.18, -248.6, -246.1, 1);
 
 
 --
@@ -151,13 +123,6 @@ INSERT INTO public.properties VALUES (10, 20.18, -248.6, -246.1, 1);
 INSERT INTO public.types VALUES (1, 'nonmetal');
 INSERT INTO public.types VALUES (2, 'metal');
 INSERT INTO public.types VALUES (3, 'metalloid');
-
-
---
--- Name: types_type_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
---
-
-SELECT pg_catalog.setval('public.types_type_id_seq', 3, true);
 
 
 --
@@ -217,11 +182,11 @@ ALTER TABLE ONLY public.types
 
 
 --
--- Name: properties properties_atomic_number_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+-- Name: properties f_key; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
 --
 
 ALTER TABLE ONLY public.properties
-    ADD CONSTRAINT properties_atomic_number_fkey FOREIGN KEY (atomic_number) REFERENCES public.elements(atomic_number);
+    ADD CONSTRAINT f_key FOREIGN KEY (atomic_number) REFERENCES public.elements(atomic_number);
 
 
 --
@@ -235,3 +200,4 @@ ALTER TABLE ONLY public.properties
 --
 -- PostgreSQL database dump complete
 --
+
